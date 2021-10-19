b4e3094e2d63e26512f88b2c15a674e8a3592f67
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-10-19 16:06:32 -0300

    create IOgarnizacaoService

---
+++ DoeVida/Core/Service/IOrganizacaoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IOrganizacaoService
+    {
+        void Edit(Organizacao organizacao);
+
+        int Insert(Organizacao organizacao);
+
+        Organizacao Get(int idOrganizacao);
+        void Delete(int idOrganizacao);
+        void GetAgendaDisponivel();
+        void Validate();
+    }
+}
