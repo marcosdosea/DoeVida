afb035359fc02def1723c667a7cb3a0e672f62a0
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2021-10-25 11:29:15 -0300

    Create DoadorController.cs

---
+++ Codigo/DoeVidaWeb/Controllers/DoadorController.cs
@@ -0,0 +1,98 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using DoeVidaWeb.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoeVidaWeb.Controllers
+{
+    public class DoadorController : Controller
+    {
+
+        IDoadorService _doadorService;
+        IMapper _mapper;
+
+        public DoadorController(IDoadorService doadorService, IMapper mapper)
+        {
+            _doadorService = doadorService;
+            _mapper = mapper;
+        }
+
+        public ActionResult Index()
+        {
+            var listDoador = _doadorService.GetAll();
+            var listDoadorModel = _mapper.Map<List<DoadorViewModel>>(listDoador);
+            return View(listDoadorModel);
+        }
+
+        // GET: DoadorController/Details/5
+        public ActionResult Details(int id)
+        {
+            Pessoa doador = _doadorService.Get(id);
+            DoadorViewModel doadorModel = _mapper.Map<DoadorViewModel>(doador);
+            return View(doadorModel);
+        }
+
+        // GET: DoadorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DoadorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DoadorViewModel doadorModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var doador = _mapper.Map<Pessoa>(doadorModel);
+                _doadorService.Insert(doador);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: DoadorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Pessoa doador = _doadorService.Get(id);
+            DoadorViewModel doadorModel = _mapper.Map<DoadorViewModel>(doador);
+            return View(doadorModel);
+        }
+
+        // POST: DoadorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, DoadorViewModel doadorModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var doador = _mapper.Map<Pessoa>(doadorModel);
+                _doadorService.Edit(doador);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: DoadorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Pessoa doador = _doadorService.Get(id);
+            DoadorViewModel doadorModel = _mapper.Map<DoadorViewModel>(doador);
+            return View(doadorModel);
+        }
+
+        // POST: DoadorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, DoadorViewModel doadorModel)
+        {
+            _doadorService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
