1ce880d85f2dc15c80ea67afe4437ae2a8f38d2c
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-10-25 10:15:38 -0300

    Mudando o nome da pasta de codigo e incluindo o dump

---
+++ Analise Projeto/DoeVida_Dump.sql
@@ -0,0 +1,284 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: doevida
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `agendamento`
+--
+
+DROP TABLE IF EXISTS `agendamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `agendamento` (
+  `idAgendamento` int NOT NULL AUTO_INCREMENT,
+  `data` date NOT NULL,
+  `tipo` enum('REMOTO','PRESENCIAL') NOT NULL,
+  `status` enum('AGENDADO','CANCELADO','ATENDIDO') NOT NULL DEFAULT 'AGENDADO',
+  `horarioAgendamento` time NOT NULL,
+  `descricao` varchar(500) DEFAULT NULL,
+  `idPessoa` int NOT NULL,
+  `idOrganizacao` int NOT NULL,
+  PRIMARY KEY (`idAgendamento`),
+  KEY `fk_Agendamento_Pessoa_idx` (`idPessoa`),
+  KEY `fk_Agendamento_Organizacao1_idx` (`idOrganizacao`),
+  CONSTRAINT `fk_Agendamento_Organizacao1` FOREIGN KEY (`idOrganizacao`) REFERENCES `organizacao` (`idOrganizacao`),
+  CONSTRAINT `fk_Agendamento_Pessoa` FOREIGN KEY (`idPessoa`) REFERENCES `pessoa` (`idPessoa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agendamento`
+--
+
+LOCK TABLES `agendamento` WRITE;
+/*!40000 ALTER TABLE `agendamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `agendamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comentario`
+--
+
+DROP TABLE IF EXISTS `comentario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comentario` (
+  `idComentario` int NOT NULL AUTO_INCREMENT,
+  `texto` varchar(200) DEFAULT NULL,
+  `data` datetime DEFAULT NULL,
+  `idSolicitacao` int NOT NULL,
+  `idPessoa` int NOT NULL,
+  PRIMARY KEY (`idComentario`),
+  KEY `fk_Comentario_Solicitacao1_idx` (`idSolicitacao`),
+  KEY `fk_Comentario_Pessoa1_idx` (`idPessoa`),
+  CONSTRAINT `fk_Comentario_Pessoa1` FOREIGN KEY (`idPessoa`) REFERENCES `pessoa` (`idPessoa`),
+  CONSTRAINT `fk_Comentario_Solicitacao1` FOREIGN KEY (`idSolicitacao`) REFERENCES `solicitacao` (`idSolicitacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentario`
+--
+
+LOCK TABLES `comentario` WRITE;
+/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `idItem` int NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `tipo` varchar(45) NOT NULL,
+  `idOrganizacao` int NOT NULL,
+  `quantidade` int NOT NULL,
+  `status` enum('DISPONIVEL','INDISPONIVEL') DEFAULT NULL,
+  PRIMARY KEY (`idItem`),
+  KEY `fk_Item_Organizacao1_idx` (`idOrganizacao`),
+  CONSTRAINT `fk_Item_Organizacao1` FOREIGN KEY (`idOrganizacao`) REFERENCES `organizacao` (`idOrganizacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organizacao`
+--
+
+DROP TABLE IF EXISTS `organizacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `organizacao` (
+  `idOrganizacao` int NOT NULL AUTO_INCREMENT,
+  `nomeOrganizacao` varchar(45) NOT NULL,
+  `cep` varchar(45) NOT NULL,
+  `uf` varchar(45) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `bairro` varchar(45) NOT NULL,
+  `logradouro` varchar(45) NOT NULL,
+  `complemento` varchar(45) DEFAULT NULL,
+  `numeroEndereco` varchar(45) NOT NULL,
+  `latitude` varchar(45) NOT NULL,
+  `longitude` varchar(45) NOT NULL,
+  `cnpj` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`idOrganizacao`)
+) ENGINE=InnoDB AUTO_INCREMENT=254 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organizacao`
+--
+
+LOCK TABLES `organizacao` WRITE;
+/*!40000 ALTER TABLE `organizacao` DISABLE KEYS */;
+INSERT INTO `organizacao` VALUES (252,'UFS ITAAA','49500-000','SE','Itabaiana','Centro','Avenida','Organização','102','100','200','12534548544'),(253,'UFS ITA BR','49500-000','SE','Itabaiana','Centro','Avenida','Organização','103','100','200','1253454854425');
+/*!40000 ALTER TABLE `organizacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pessoa`
+--
+
+DROP TABLE IF EXISTS `pessoa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pessoa` (
+  `idPessoa` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `dataNascimento` date NOT NULL,
+  `status` enum('ATIVO','INATIVO') NOT NULL DEFAULT 'ATIVO',
+  `tipo` enum('SERVIDOR','DOADOR','MOTORISTA') NOT NULL,
+  `latitude` varchar(45) DEFAULT NULL,
+  `longitude` varchar(45) DEFAULT NULL,
+  `cep` varchar(9) NOT NULL,
+  `logradouro` varchar(100) NOT NULL,
+  `numeroEndereco` varchar(45) NOT NULL,
+  `bairro` varchar(45) NOT NULL,
+  `complemento` varchar(100) DEFAULT NULL,
+  `uf` varchar(2) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `idPessoa1` int NOT NULL,
+  PRIMARY KEY (`idPessoa`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `cpf_UNIQUE` (`cpf`),
+  KEY `fk_Pessoa_Pessoa1_idx` (`idPessoa1`),
+  CONSTRAINT `fk_Pessoa_Pessoa1` FOREIGN KEY (`idPessoa1`) REFERENCES `pessoa` (`idPessoa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoa`
+--
+
+LOCK TABLES `pessoa` WRITE;
+/*!40000 ALTER TABLE `pessoa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pessoa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solicitacao`
+--
+
+DROP TABLE IF EXISTS `solicitacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `solicitacao` (
+  `idSolicitacao` int NOT NULL AUTO_INCREMENT,
+  `data` datetime DEFAULT NULL,
+  `status` enum('PROBLEMA NA ENTREGA','SOLICITADO','RECUSADO','APROVADO') NOT NULL,
+  `idPessoa` int NOT NULL,
+  `idOrganizacao` int NOT NULL,
+  `quantidade` int NOT NULL,
+  PRIMARY KEY (`idSolicitacao`),
+  KEY `fk_Solicitacao_Pessoa1_idx` (`idPessoa`),
+  KEY `fk_Solicitacao_Organizacao1_idx` (`idOrganizacao`),
+  CONSTRAINT `fk_Solicitacao_Organizacao1` FOREIGN KEY (`idOrganizacao`) REFERENCES `organizacao` (`idOrganizacao`),
+  CONSTRAINT `fk_Solicitacao_Pessoa1` FOREIGN KEY (`idPessoa`) REFERENCES `pessoa` (`idPessoa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitacao`
+--
+
+LOCK TABLES `solicitacao` WRITE;
+/*!40000 ALTER TABLE `solicitacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solicitacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solicitacaoitem`
+--
+
+DROP TABLE IF EXISTS `solicitacaoitem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `solicitacaoitem` (
+  `idSolicitacaoItem` int NOT NULL,
+  `idSolicitacao` int NOT NULL,
+  `idItem` int NOT NULL,
+  PRIMARY KEY (`idItem`,`idSolicitacao`),
+  KEY `fk_SolicitacaoItem_Solicitacao1_idx` (`idSolicitacao`),
+  KEY `fk_SolicitacaoItem_Item1_idx` (`idItem`),
+  CONSTRAINT `fk_SolicitacaoItem_Item1` FOREIGN KEY (`idItem`) REFERENCES `item` (`idItem`),
+  CONSTRAINT `fk_SolicitacaoItem_Solicitacao1` FOREIGN KEY (`idSolicitacao`) REFERENCES `solicitacao` (`idSolicitacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitacaoitem`
+--
+
+LOCK TABLES `solicitacaoitem` WRITE;
+/*!40000 ALTER TABLE `solicitacaoitem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solicitacaoitem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vagashorarios`
+--
+
+DROP TABLE IF EXISTS `vagashorarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vagashorarios` (
+  `idVagasHorarios` int NOT NULL AUTO_INCREMENT,
+  `diaSemana` enum('SEGUNDA-FEIRA','TERÇA-FEIRA','QUARTA-FEIRA','QUINTA-FEIRA','SEXTA-FEIRA','SABADO','DOMINGO') DEFAULT NULL,
+  `horaInicio` time DEFAULT NULL,
+  `horaFinal` time DEFAULT NULL,
+  `numeroVagas` int DEFAULT NULL,
+  `idOrganizacao` int NOT NULL,
+  PRIMARY KEY (`idVagasHorarios`),
+  KEY `fk_VagasHorarios_Organizacao1_idx` (`idOrganizacao`),
+  CONSTRAINT `fk_VagasHorarios_Organizacao1` FOREIGN KEY (`idOrganizacao`) REFERENCES `organizacao` (`idOrganizacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vagashorarios`
+--
+
+LOCK TABLES `vagashorarios` WRITE;
+/*!40000 ALTER TABLE `vagashorarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vagashorarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-25 10:13:39
