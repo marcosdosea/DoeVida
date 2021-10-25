debca4d2863cdb42afe037d3f76986817681037e
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2021-10-25 10:30:25 -0300

    create IDoadorService

---
+++ Codigo/Core/Service/IDoadorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IDoadorService
+    {
+        int Insert(Pessoa pessoa);
+        void Edit(Pessoa pessoa);
+        IQueryable<Pessoa> GetQuery();
+        Pessoa Get(int idPessoa);
+        IEnumerable<Pessoa> GetAll();
+        IEnumerable<Pessoa> GetAllOrderByName();
+        IEnumerable<Pessoa> GetByNameContained(string name);
+        void Delete(int idPessoa);
+        void Validate();
+    }
+}
