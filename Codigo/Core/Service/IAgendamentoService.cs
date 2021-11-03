024efb538ffb1595a69511594b536f7512f4345f
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-11-03 10:30:44 -0300

    Foi implementado a interface do service e a sua implementação

---
+++ Codigo/Core/Service/IAgendamentoService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IAgendamentoService
+    {
+        void Edit(Agendamento agendamento);
+
+        int Insert(Agendamento agendamento);
+
+        IQueryable<Agendamento> GetQuery();
+        Agendamento Get(int idAgendamento);
+        IEnumerable<Agendamento> GetAll();
+        IEnumerable<Agendamento> GetAllOrderByName();
+        IEnumerable<Agendamento> GetByNameContained(string name);
+        void Delete(int idAgendamento);
+        void Validate();
+
+        // todo: GetAgendaDisponivel
+    }
+}
---
+++ Codigo/Service/AgendamentoService.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Service;
+using Core;
+
+namespace Service
+{
+    class AgendamentoService : IAgendamentoService
+    {
+        private readonly DoeVidaDbContext _context;
+        public AgendamentoService(DoeVidaDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+		/// Insere uma nova organização na base de dados
+		/// </summary>
+		/// <param name="agendamento">dados do agendamento</param>
+		/// <returns>Retorna o Id da agendamento inserida</returns>
+        public int Insert(Agendamento agendamento)
+        {
+            _context.Agendamento.Add(agendamento);
+            _context.SaveChanges();
+            return agendamento.IdAgendamento;
+        }
+
+        /// <summary>
+		/// Atualiza os dados do agendamento na base de dados
+		/// </summary>
+		/// <param name="agendamento">dados do agendamento</param>
+        public void Edit(Agendamento agendamento)
+        {
+            _context.Update(agendamento);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Remove uma organização da base de dados
+        /// </summary>
+        /// <param name="idAgendamento">identificador do agendamento</param>
+        public void Delete(int idAgendamento)
+        {
+            var _agendamento = _context.Agendamento.Find(idAgendamento);
+            _context.Agendamento.Remove(_agendamento);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+		/// Consulta genérica aos dados do agendamento
+		/// </summary>
+		/// <returns></returns>
+		public IQueryable<Agendamento> GetQuery()
+        {
+            //IQueryable<Agendamento> listaAgendamento = _context.Agendamento;
+            var query = from agendamento in _context.Agendamento
+                        select agendamento;
+            return query;
+        }
+
+        /// <summary>
+        /// Obtém todos os Agendamentos
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Agendamento> GetAll()
+        {
+            return GetQuery();
+        }
+
+        /// <summary>
+		/// Obtém pelo identificador do agendamento
+		/// </summary>
+		/// <param name="idAgendamento"></param>
+		/// <returns>Retorna uma Agendamento</returns>
+        public Agendamento Get(int idAgendamento)
+        {
+            // todo: execption?
+            var _agendamento = _context.Agendamento.Find(idAgendamento);
+            return _agendamento;
+        }
+
+        /// <summary>
+		/// Obter todos os agendamentos ordenando pelo nome do doador
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<Agendamento> GetAllOrderByName()
+        {
+            var query = from A in _context.Agendamento
+                        join P in _context.Pessoa
+                        on A.IdPessoa equals P.IdPessoa
+                        orderby P.Nome
+                        select A;
+            return query;
+        }
+
+        /// <summary>
+		/// Obter todos os agendamentos que contem o nome
+		/// </summary>
+        /// <param name="name">nome do agendamento</param>
+		/// <returns></returns>
+        public IEnumerable<Agendamento> GetByNameContained(string name)
+        {
+            var query = from A in _context.Agendamento
+                        join P in _context.Pessoa
+                        on A.IdPessoa equals P.IdPessoa
+                        where P.Nome.Contains(name)
+                        select A;
+            return query;
+        }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
