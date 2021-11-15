c0816b5214b1ba0aba63adf23a13daca23739f5f
Victor <victorluz.lima@gmail.com>
2021-11-15 17:17:19 -0300

    issue#67 adicionado testes unitarios em Item

---
+++ Codigo/DoeVidaWebTests/Controllers/ItemControllerTests.cs
@@ -0,0 +1,191 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using DoeVidaWeb.Mappers;
+using DoeVidaWeb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+
+namespace DoeVidaWeb.Controllers.Tests
+{
+    [TestClass()]
+    public class ItemControllerTests
+    {
+        private static ItemController controller;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            // Arrange
+            var mockService = new Mock<IItemService>();
+
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new ItemProfile())).CreateMapper();
+
+            mockService.Setup(service => service.GetAll())
+                .Returns(GetTestItem());
+
+            mockService.Setup(service => service.Get(1))
+                .Returns(GetTargetItem());
+            mockService.Setup(service => service.Edit(It.IsAny<Item>()))
+                .Verifiable();
+            mockService.Setup(service => service.Insert(It.IsAny<Item>()))
+                .Verifiable();
+
+            controller = new ItemController(mockService.Object, mapper);
+        }
+
+        public void CreateTest()
+        {
+            // Act
+            var result = controller.Create();
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+        public void CreateTest_Valid()
+        {
+            // Act
+            var result = controller.Create(GetNewItem());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+        public void CreateTest_InValid()
+        {
+            // Arrange
+            controller.ModelState.AddModelError("Nome", "Campo requerido");
+
+            // Act
+            var result = controller.Create(GetNewItem());
+
+            // Assert
+            Assert.AreEqual(1, controller.ModelState.ErrorCount);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        public void EditTest_Get()
+        {
+            // Act
+            var result = controller.Edit(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(ItemViewModel));
+            ItemViewModel itemViewmodel = (ItemViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Recipiente Tipo A", itemViewmodel.Nome);
+            Assert.AreEqual("DISPONIVEL", itemViewmodel.Status);
+        }
+
+        [TestMethod()]
+        public void EditTest_Post()
+        {
+            // Act
+            var result = controller.Edit(GetTargetItemViewModel().IdItem, GetTargetItemViewModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+        public void DeleteTest_Get()
+        {
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(ItemViewModel));
+            ItemViewModel itemviewmodel = (ItemViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Recipiente Tipo A", itemviewmodel.Nome);
+        }
+
+
+        [TestMethod()]
+        public void DeleteTest_Post()
+        {
+            // Act
+            var result = controller.Delete(GetTargetItemViewModel().IdItem, GetTargetItemViewModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+        private static IEnumerable<Item> GetTestItem()
+        {
+            return new List<Item>
+            {
+                new Item
+                {
+                    IdItem = 1,
+                    Nome = "Recipiente Tipo A",
+                    IdOrganizacao = 253,
+                    Quantidade =    2,
+                    Status = "DISPONIVEL",
+                    Tipo =  "TESTE"
+                },
+                new Item
+                {
+                    IdItem = 2,
+                    Nome = "Recipiente Tipo b",
+                    IdOrganizacao = 253,
+                    Quantidade =    0,
+                    Status = "INDISPONIVEL",
+                    Tipo =  "TESTE"
+                },
+
+            };
+        }
+        private static ItemViewModel GetNewItem()
+        {
+            return new ItemViewModel
+            {
+                IdItem = 1,
+                Nome = "Recipiente Tipo A",
+                IdOrganizacao = 253,
+                Quantidade = 2,
+                Status = "DISPONIVEL",
+                Tipo = "TESTE"
+            };
+
+        }
+        private static Item GetTargetItem()
+        {
+            return new Item
+            {
+                IdItem = 1,
+                Nome = "Recipiente Tipo A",
+                IdOrganizacao = 253,
+                Quantidade = 2,
+                Status = "DISPONIVEL",
+                Tipo = "TESTE"
+            };
+        }
+        private static ItemViewModel GetTargetItemViewModel()
+        {
+            return new ItemViewModel
+            {
+                IdItem = 1,
+                Nome = "Recipiente Tipo A",
+                IdOrganizacao = 253,
+                Quantidade = 2,
+                Status = "DISPONIVEL",
+                Tipo = "TESTE"
+            };
+        }
+
+
+    }
+}
---
+++ Codigo/ServiceTests/ItemServiceTests.cs
@@ -0,0 +1,158 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class ItemServiceTest
+    {
+        private DoeVidaDbContext _context;
+        private IItemService _itemService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Arrange
+            var builder = new DbContextOptionsBuilder<DoeVidaDbContext>();
+            builder.UseInMemoryDatabase("doevida");
+            var options = builder.Options;
+
+            _context = new DoeVidaDbContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var itens = new List<Item>
+                {
+                 new Item
+                {
+                    IdItem = 1,
+                    Nome = "Recipiente Tipo A",
+                    IdOrganizacao = 253,
+                    Quantidade =    2,
+                    Status = "DISPONIVEL",
+                    Tipo =  "TESTE"
+                },
+                 new Item
+                {
+                    IdItem = 2,
+                    Nome = "Recipiente Tipo A",
+                    IdOrganizacao = 253,
+                    Quantidade =    2,
+                    Status = "DISPONIVEL",
+                    Tipo =  "TESTE"
+                },
+                 new Item
+                {
+                    IdItem = 3,
+                    Nome = "Recipiente Tipo A",
+                    IdOrganizacao = 253,
+                    Quantidade =    2,
+                    Status = "DISPONIVEL",
+                    Tipo =  "TESTE"
+                }
+                };
+
+            _context.AddRange(itens);
+            _context.SaveChanges();
+
+            _itemService = new ItemService(_context);
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            // Act
+            _itemService.Insert(new Item
+            {
+                IdItem = 3,
+                Nome = "Recipiente Tipo A",
+                IdOrganizacao = 253,
+                Quantidade = 2,
+                Status = "DISPONIVEL",
+                Tipo = "TESTE"
+            });
+            // Assert
+            Assert.AreEqual(4, _itemService.GetAll().Count());
+            var item = _itemService.Get(4);
+            Assert.AreEqual(1, item.IdItem);
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            var item = _itemService.Get(3);
+            item.Tipo = "TESTE";
+            _itemService.Edit(item);
+            item = _itemService.Get(3);
+            Assert.AreEqual("TESTE", item.Tipo);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            // Act
+            _itemService.Delete(2);
+            // Assert
+            Assert.AreEqual(2, _itemService.GetAll().Count());
+            var item = _itemService.Get(2);
+            Assert.AreEqual(null, item);
+        }
+
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            // Act
+            var listaItem = _itemService.GetAll();
+            // Assert
+            Assert.IsInstanceOfType(listaItem, typeof(IEnumerable<Item>));
+            Assert.IsNotNull(listaItem);
+            Assert.AreEqual(3, listaItem.Count());
+            Assert.AreEqual(1, listaItem.First().IdItem);
+            Assert.AreEqual("Recipiente Tipo A", listaItem.First().Nome);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            var item = _itemService.Get(1);
+            Assert.IsNotNull(item);
+            Assert.AreEqual("Recipiente Tipo A", item.Nome);
+            Assert.AreEqual("TESTE", item.Tipo);
+        }
+
+
+
+        [TestMethod()]
+        public void GetAllOrderByNameTest()
+        {
+            // Act
+            var ListaItem = _itemService.GetAllOrderByName();
+            // Assert
+            Assert.IsInstanceOfType(ListaItem, typeof(IEnumerable<Item>));
+            Assert.IsNotNull(ListaItem);
+            Assert.AreEqual(3, ListaItem.Count());
+            Assert.AreEqual(1, ListaItem.First().IdItem);
+        }
+
+        [TestMethod()]
+        public void GetByNameContainedTest()
+        {
+            // Act
+            var ListaItem = _itemService.GetByNameContained("3");
+            // Assert
+            Assert.IsInstanceOfType(ListaItem, typeof(IEnumerable<Item>));
+            Assert.IsNotNull(ListaItem);
+            Assert.AreEqual(1, ListaItem.Count());
+            Assert.AreEqual(3, ListaItem.First().IdOrganizacao);
+        }
+
+    }
+}
