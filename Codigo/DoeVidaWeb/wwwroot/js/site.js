2d5fc44f5240865b16101954e6fd57bbfa03757a
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-11-26 15:40:15 -0300

    Foi alterado o codigo js da view para o site.js

--- Codigo/DoeVidaWeb/wwwroot/js/site.js
+++ Codigo/DoeVidaWeb/wwwroot/js/site.js
@@ -1,4 +1,150 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function search_cep(valor) {
 
-// Write your JavaScript code.
+    if (valor.length < 8)
+        return;
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('rua').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            document.getElementById('uf').value = "...";
+
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=complete_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            clear_form();
+            alert("Formato de CEP inválido.");
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        clear_form();
+    }
+};
+
+function clear_form() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('cidade').value = ("");
+    document.getElementById('uf').value = ("");
+}
+
+function complete_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('rua').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('uf').value = (conteudo.uf);
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        clear_form();
+        alert("CEP não encontrado.");
+    }
+}
+
+function mask(o, f) {
+    v_obj = o
+    v_fun = f
+    setTimeout("execmask()", 1)
+}
+
+function execmask() {
+    v_obj.value = v_fun(v_obj.value)
+}
+
+function mask_phone(v) {
+    v = v.replace(/\D/g, ""); //Remove tudo o que não é dígito
+    v = v.replace(/^(\d{2})(\d)/g, "($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v = v.replace(/(\d)(\d{4})$/, "$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+
+function get_element_id(el) {
+    return document.getElementById(el);
+}
+
+window.onload = function () {
+    get_element_id('telefone').onkeyup = function () {
+        mask(this, mask_phone);
+    }
+}
+
+function is_cpf(c) {
+
+    if ((c = c.replace(/[^\d]/g, "")).length != 11)
+        return false
+
+    if (c == "00000000000")
+        return false;
+
+    var r;
+    var s = 0;
+
+    for (i = 1; i <= 9; i++)
+        s = s + parseInt(c[i - 1]) * (11 - i);
+
+    r = (s * 10) % 11;
+
+    if ((r == 10) || (r == 11))
+        r = 0;
+
+    if (r != parseInt(c[9]))
+        return false;
+
+    s = 0;
+
+    for (i = 1; i <= 10; i++)
+        s = s + parseInt(c[i - 1]) * (12 - i);
+
+    r = (s * 10) % 11;
+
+    if ((r == 10) || (r == 11))
+        r = 0;
+
+    if (r != parseInt(c[10]))
+        return false;
+
+    return true;
+}
+
+function function_mask(objeto, mask) {
+    obj = objeto
+    masc = mask
+    setTimeout("function_maskEx()", 1)
+}
+
+function function_maskEx() {
+    obj.value = masc(obj.value)
+}
+
+function mask_CPF(cpf) {
+    cpf = cpf.replace(/\D/g, "")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    return cpf
+}
