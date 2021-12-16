8f27b9d8068ecc439449774e95a1b2f4c38723a9
davi cruz <daviondejesus@gmail.com>
2021-12-15 21:55:47 -0300

    iniciar na tela de login

--- Codigo/DoeVidaWeb/Controllers/HomeController.cs
+++ Codigo/DoeVidaWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using DoeVidaWeb.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace DoeVidaWeb.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
